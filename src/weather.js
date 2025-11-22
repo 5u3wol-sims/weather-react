ac9faf03cd1db9874bc6058c257d84cc27d91fce
SuzannaW-web <suzannawallis@gmail.com>
2025-11-22 20:34:00 +0000

    Added axios integration so it shows the weather of a city

---
+++ src/weather.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.city} is ${response.data.temperature.current} degrees C`
+    );
+  }
+
+  let apiKey = "3o03ae1d8at693bf40d8fb4c10733055&units=metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="#4fa94d"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{ margin: "20px" }}
+      wrapperClass="custom-loader"
+      visible={true}
+    />
+  );
+}
